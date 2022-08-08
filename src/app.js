c44b515e1c100f98abd1d1a3b15d0f1e961ddc5e
Matheus Balcky <matheuscabal821@gmail.com>
2022-08-08 20:58:52 -0300

    Files Structure

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors'
+import dotenv from 'dotenv'; dotenv.config();
+//import route from './routes/routes.js';
+
+
+const app = express();
+app.use(express.json(), cors());
+
+//app.use(route)
+
+
+app.listen(process.env.PORT, console.log(`Server running at port ${process.env.PORT}`));
