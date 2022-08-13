db137d121cc59be1e17a1d1b8f3752e4b7a7a7dc
Augusto Lopes <augustolfp@gmail.com>
2022-08-13 16:20:31 -0300

    Implements endpoint that returns the 20 newest posts

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -5,13 +5,25 @@ export async function publishPost(req, res) {
     const userId = 1;
     const {postUrl, postDescription} = req.body;
 
-    try{
+    try {
         const query = await db.query(`INSERT INTO posts ("url","text","userId") VALUES ($1,$2,$3)`,[postUrl, postDescription, userId]);
         res.sendStatus(201);
         return;
     }
     catch(error) {
         res.status(404).send(error);
+        return;
+    }
+}
+
+export async function getPosts(req, res) {
+    try {
+        const query = await db.query(`SELECT * FROM posts ORDER BY posts."createdAt" DESC LIMIT 20`);
+        res.status(200).send(query.rows);
+        return;
+    }
+    catch(error) {
+        res.status(400).send("Ocorreu um erro");
         return;
     }
 }
