5b8840ef57ffc3243b1661e4c4f9338e107a5b16
Augusto Lopes <augustolfp@gmail.com>
2022-08-13 16:43:24 -0300

    Adds a username column on GET /feed endpoint response

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -18,7 +18,8 @@ export async function publishPost(req, res) {
 
 export async function getPosts(req, res) {
     try {
-        const query = await db.query(`SELECT * FROM posts ORDER BY posts."createdAt" DESC LIMIT 20`);
+        const query = await db.query(`SELECT p.id AS id, u.username AS username, p.url AS url, p.text AS text, p."createdAt" AS "createdAt" 
+        FROM posts p JOIN users u ON u.id = p."userId" ORDER BY "createdAt" DESC LIMIT 20`);
         res.status(200).send(query.rows);
         return;
     }
